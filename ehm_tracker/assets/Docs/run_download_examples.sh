f07cd4cbc1b746623d18a89f0c5253db47080a0b
quang-ngh <quangngcs@gmail.com>
2026-02-13 22:16:56 -0500

    add assets

---
+++ ehm_tracker/assets/Docs/run_download_examples.sh
@@ -0,0 +1,4 @@
+pip install gdown
+gdown --id 1clwXCV92T5YtAXsI7k2pqf40PnvLmF_t -O assets/examples.zip
+unzip assets/examples.zip -d assets
+rm assets/examples.zip
---
+++ ehm_tracker/assets/Docs/run_download_pretrained.sh
@@ -0,0 +1,4 @@
+pip install gdown
+gdown --id 1g_4YKQvLSWo8yzYHgNstr91RCD4rne8p -O pretrained.zip
+unzip pretrained.zip -d .
+rm pretrained.zip
